32da4a10bc9b07c9982777520e428cca509180ae
boyerjason700 <74840026+boyerjason700@users.noreply.github.com>
2021-08-29 16:35:13 -0700

    working table

--- static/js/app.js
+++ static/js/app.js
@@ -20,16 +20,15 @@ function buildTable(data) {
         Object.values(dataRow).forEach((val) => {
             let cell = row.append("td");
             cell.text(val);
-            }
-        );
+        });
     });
-}
+};
 
 
 function handleClick() {
     // Grab the datetime value from the filter
     let date = d3.select("#datetime").property("value");
-    let filterData = tableData;
+    let filteredData = tableData;
 
     // Check to see if a date was entered and filter the
     // data using that date.
@@ -42,7 +41,7 @@ function handleClick() {
     // @NOTE: If no date was entered, then filteredData will
     // just be the original tableData.
     buildTable(filteredData);
-}
+};
 
 // Attach an event to listen for the form button
 d3.selectAll("#filter-btn").on("click", handleClick);
